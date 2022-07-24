f7b9a7a2c1deb22e1c9827ea2a763e04604a9387
Nico <nicolas.otremba@web.com>
2022-07-24 21:48:58 +0200

    moved keybind autocomplete to view, changed colors

---
+++ src/keybind_autocomplete.nim
@@ -0,0 +1,34 @@
+import std/[strformat, strutils, algorithm, math, logging, unicode, sequtils]
+import print
+import input, document, document_editor, events
+
+var logger = newConsoleLogger()
+
+type KeybindAutocompletion* = ref object of DocumentEditor
+  discard
+
+method getEventHandlers*(self: KeybindAutocompletion): seq[EventHandler] =
+  return @[self.eventHandler]
+
+proc handleAction(self: KeybindAutocompletion, action: string, arg: string): EventResponse =
+  # echo "handleAction ", action, " '", arg, "'"
+  case action
+  of "cursor.up": discard
+  of "cursor.down": discard
+  else:
+    logger.log(lvlError, "[KeybindAutocomplete] Unknown action '$1 $2'" % [action, arg])
+  return Handled
+
+proc handleInput(self: KeybindAutocompletion, input: string): EventResponse =
+  return Handled
+
+proc newKeybindAutocompletion*(): DocumentEditor =
+  let editor = KeybindAutocompletion(eventHandler: nil)
+  editor.eventHandler = eventHandler2:
+    command "<UP>", "cursor.up"
+    command "<DOWN>", "cursor.down"
+    onAction:
+      editor.handleAction action, arg
+    onInput:
+      editor.handleInput input
+  return editor
