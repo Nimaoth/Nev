fb7cd7c9d57bb7fcbe1d740948b06f61ae5c18ea
Nimaoth <nicolas.otremba@web.de>
2022-12-21 13:06:15 +0100

    removed old files

--- src/test.nim
+++
@@ -1,8 +0,0 @@
-import std/[strformat, bitops, strutils, tables, algorithm, math]
-import boxy, opengl, times, windy
-import monitors
-import sugar
-import input
-
-let typeface = readTypeface("fonts/FiraCode-Regular.ttf")
-let typeface2 = readTypeface("fonts/Noto_Emoji/static/NotoEmoji-Regular.ttf")
