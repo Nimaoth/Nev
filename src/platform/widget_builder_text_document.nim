e8fd43dfc04f20320df17701699f16f725b494a9
Nimaoth <nicolas.otremba@web.de>
2023-02-15 21:31:03 +0100

    added rendering of selections and cursor in new renderer

--- src/platform/widget_builder_text_document.nim
+++ src/platform/widget_builder_text_document.nim
@@ -1,4 +1,4 @@
-import std/[strformat]
+import std/[strformat, tables, sugar, algorithm, sequtils]
 import util, editor, document_editor, text_document, custom_logger, widgets, platform, timer, theme
 import scripting_api except DocumentEditor, TextDocumentEditor, AstDocumentEditor
 import vmath, bumpy, chroma
@@ -6,12 +6,43 @@ import vmath, bumpy, chroma
 # Mark this entire file as used, otherwise we get warnings when importing it but only calling a method
 {.used.}
 
+proc clampToLine(selection: Selection, line: int, lineLength: int): tuple[first: int, last: int] =
+  result.first = if selection.first.line < line: 0 elif selection.first.line == line: selection.first.column else: lineLength
+  result.last = if selection.last.line < line: 0 elif selection.last.line == line: selection.last.column else: lineLength
+
+proc renderTextHighlight(panel: WPanel, app: Editor, startOffset: float, endOffset: float, line: int, startIndex: int, selection: Selection, selectionClamped: tuple[first: int, last: int], part: StyledText, color: Color) =
+  ## Fills a selection rect in the given color
+  var left, right: float
+  if startIndex < selectionClamped.last and startIndex + part.text.len > selectionClamped.first and part.text.len > 0:
+    left = startOffset + max(0, selectionClamped.first - startIndex).float32 / (part.text.len.float32 - 0) * (endOffset - startOffset)
+    right = startOffset + min(part.text.len, selectionClamped.last - startIndex).float32 / (part.text.len.float32 - 0) * (endOffset - startOffset)
+  elif part.text.len == 0 and selection.contains((line, startIndex)) and not selection.isEmpty:
+    left = 0
+    right = ceil(app.platform.charWidth * 0.5)
+  else:
+    return
+  panel.children.add(WPanel(
+    anchor: (vec2(0, 0), vec2(0, 1)),
+    left: left,
+    right: right,
+    fillBackground: true,
+    backgroundColor: color,
+    lastHierarchyChange: panel.lastHierarchyChange
+  ))
+
+proc renderTextHighlight(panel: WPanel, app: Editor, startOffset: float, endOffset: float, line: int, startIndex: int, selections: openArray[Selection], selectionClamped: openArray[tuple[first: int, last: int]], part: StyledText, color: Color) =
+  ## Fills selections rect in the given color
+  for i in 0..<selections.len:
+    renderTextHighlight(panel, app, startOffset, endOffset, line, startIndex, selections[i], selectionClamped[i], part, color)
+
 method updateWidget*(self: TextDocumentEditor, app: Editor, widget: WPanel, frameIndex: int) =
   let lineHeight = app.platform.lineHeight
   let totalLineHeight = app.platform.totalLineHeight
   let charWidth = app.platform.charWidth
 
   self.lastContentBounds = widget.lastBounds
+
+  let textColor = app.theme.color("editor.foreground", rgb(225, 200, 200))
 
   var headerPanel: WPanel
   var headerPart1Text: WText
@@ -21,10 +52,10 @@ method updateWidget*(self: TextDocumentEditor, app: Editor, widget: WPanel, fram
     headerPanel = WPanel(anchor: (vec2(0, 0), vec2(1, 0)), left: 1, bottom: totalLineHeight, lastHierarchyChange: frameIndex, fillBackground: true, backgroundColor: color(0, 0, 0))
     widget.children.add(headerPanel)
 
-    headerPart1Text = WText(text: "", sizeToContent: true, anchor: (vec2(0, 0), vec2(0, 1)), lastHierarchyChange: frameIndex, foregroundColor: color(0, 1, 0))
+    headerPart1Text = WText(text: "", sizeToContent: true, anchor: (vec2(0, 0), vec2(0, 1)), lastHierarchyChange: frameIndex, foregroundColor: textColor)
     headerPanel.children.add(headerPart1Text)
 
-    headerPart2Text = WText(text: "", sizeToContent: true, anchor: (vec2(1, 0), vec2(1, 1)), pivot: vec2(1, 0), lastHierarchyChange: frameIndex, foregroundColor: color(0, 0, 1))
+    headerPart2Text = WText(text: "", sizeToContent: true, anchor: (vec2(1, 0), vec2(1, 1)), pivot: vec2(1, 0), lastHierarchyChange: frameIndex, foregroundColor: textColor)
     headerPanel.children.add(headerPart2Text)
 
     contentPanel = WPanel(anchor: (vec2(0, 0), vec2(1, 1)), top: totalLineHeight, lastHierarchyChange: frameIndex, fillBackground: true, backgroundColor: color(0, 0, 0))
@@ -89,7 +120,21 @@ method updateWidget*(self: TextDocumentEditor, app: Editor, widget: WPanel, fram
       self.previousBaseIndex -= 1
       self.scrollOffset -= totalLineHeight
 
-  let textColor = app.theme.color("editor.foreground", rgb(225, 200, 200))
+  var selectionsPerLine = initTable[int, seq[Selection]]()
+  for s in self.selections:
+    let sn = s.normalized
+    for line in sn.first.line..sn.last.line:
+      selectionsPerLine.mgetOrPut(line, @[]).add s
+
+  var highlightsPerLine = self.searchResults
+
+  let lineNumbers = self.lineNumbers.get getOption[LineNumbers](app, "editor.text.line-numbers", LineNumbers.Absolute)
+  let maxLineNumber = case lineNumbers
+    of LineNumbers.Absolute: self.previousBaseIndex + ((contentPanel.lastBounds.h - self.scrollOffset) / totalLineHeight).int
+    of LineNumbers.Relative: 99
+    else: 0
+  let maxLineNumberLen = ($maxLineNumber).len + 1
+  let cursorLine = self.selection.last.line
 
   # Update content
   proc renderLine(i: int, down: bool): bool =
@@ -104,14 +149,47 @@ method updateWidget*(self: TextDocumentEditor, app: Editor, widget: WPanel, fram
 
     var styledText = self.document.getStyledText(i)
 
-    var lineWidget = WHorizontalList(anchor: (vec2(0, 0), vec2(1, 0)), left: 1, right: -1, top: top, bottom: top + totalLineHeight, lastHierarchyChange: frameIndex)
+    let selectionsNormalizedOnLine = selectionsPerLine.getOrDefault(i, @[]).map (s) => s.normalized
+    let selectionsClampedOnLine = selectionsNormalizedOnLine.map (s) => s.clampToLine(i, styledText.len)
+    let highlightsNormalizedOnLine = highlightsPerLine.getOrDefault(i, @[]).map (s) => s.normalized
+    let highlightsClampedOnLine = highlightsNormalizedOnLine.map (s) => s.clampToLine(i, styledText.len)
+
+    var lineWidget = WPanel(anchor: (vec2(0, 0), vec2(1, 0)), left: 1, right: -1, top: top, bottom: top + totalLineHeight, lastHierarchyChange: frameIndex)
 
     var startOffset = 0.0
+    var startIndex = 0
     for partIndex, part in styledText.parts:
       let width = part.text.len.float * charWidth
+
+      # Draw background if selected
+      let selectionColor = app.theme.color("selection.background", rgb(200, 200, 200))
+      renderTextHighlight(lineWidget, app, startOffset, startOffset + width, i, startIndex, selectionsNormalizedOnLine, selectionsClampedOnLine, part, selectionColor)
+
+      let highlightColor = app.theme.color(@["editor.rangeHighlightBackground"], rgb(200, 200, 200))
+      renderTextHighlight(lineWidget, app, startOffset, startOffset + width, i, startIndex, highlightsNormalizedOnLine, highlightsClampedOnLine, part, highlightColor)
+
+      let isWide = getOption[bool](app, self.getContextWithMode("editor.text.cursor.wide"))
+      let cursorWidth = if isWide: 1.0 else: 0.2
+
+      # Set last cursor pos if it's contained in this part
+      let cursorColor = app.theme.color(@["editorCursor.foreground", "foreground"], rgba(255, 255, 255, 127))
+      for selection in selectionsPerLine.getOrDefault(i, @[]):
+        if selection.last.line == i and selection.last.column >= startIndex and selection.last.column <= startIndex + part.text.len:
+          let offsetFromPartStart = if part.text.len == 0: 0.0 else: (selection.last.column - startIndex).float32 / (part.text.len.float32) * width
+          lineWidget.children.add(WPanel(
+            anchor: (vec2(0, 0), vec2(0, 1)),
+            left: startOffset + offsetFromPartStart,
+            right: startOffset + offsetFromPartStart + cursorWidth * charWidth,
+            fillBackground: true,
+            backgroundColor: cursorColor,
+            lastHierarchyChange: frameIndex
+          ))
+
       let color = if part.scope.len == 0: textColor else: app.theme.tokenColor(part.scope, rgb(255, 200, 200))
       var partWidget = WText(text: part.text, anchor: (vec2(0, 0), vec2(0, 1)), left: startOffset, right: startOffset + width, foregroundColor: color, lastHierarchyChange: frameIndex)
+
       startOffset += width
+      startIndex += part.text.len
 
       lineWidget.children.add(partWidget)
 
