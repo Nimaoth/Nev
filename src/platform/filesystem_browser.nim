4ce071a7ac33fc1c445758617b0a0dec06518218
Nimaoth <nicolas.otremba@web.de>
2023-02-18 21:51:06 +0100

    added file system abstraction

---
+++ src/platform/filesystem_browser.nim
@@ -0,0 +1,18 @@
+import std/[dom]
+
+import filesystem
+
+type FileSystemBrowser* = ref object of FileSystem
+  discard
+
+method loadFile*(self: FileSystemBrowser, path: string): string =
+  discard
+
+method saveFile*(self: FileSystemBrowser, path: string, content: string) =
+  discard
+
+method loadApplicationFile*(self: FileSystemBrowser, name: string): string =
+  discard
+
+method saveApplicationFile*(self: FileSystemBrowser, name: string, content: string) =
+  discard
