7f45e622f998ed1e4a9cd8db7bdbf51cc949842b
Nimaoth <nicolas.otremba@web.de>
2023-06-03 21:47:03 +0200

    added nim version of absytree_server

---
+++ src/servers/absytree_server.nim
@@ -0,0 +1,28 @@
+import std/[os, strutils]
+import custom_async, util
+import languages_server, workspace_server
+
+when isMainModule:
+  const workspaceServerPortArg = "--workspace-port:"
+  const languagesServerPortArg = "--languages-port:"
+  var workspaceServerPort = 3000
+  var languagesServerPort = 3001
+
+  for arg in commandLineParams():
+    if arg.startsWith(workspaceServerPortArg):
+      workspaceServerPort = arg[workspaceServerPortArg.len..^1].parseInt
+    elif arg.startsWith(languagesServerPortArg):
+      languagesServerPort = arg[languagesServerPortArg.len..^1].parseInt
+    else:
+      echo "Unexpected argument '", arg, "'"
+      quit(1)
+
+  if languagesServerPort == workspaceServerPort:
+    echo "Can't use port ", languagesServerPort, " for both servers"
+    quit(1)
+
+  var workspace = runWorkspaceServer(Port(workspaceServerPort))
+  var languages = runLanguagesServer(Port(languagesServerPort))
+
+  while not workspace.finished and not languages.finished:
+    poll()
---
+++ src/servers/workspace_server.nim
@@ -0,0 +1,109 @@
+import std/[os, asynchttpserver, strutils, strformat, uri, asyncfile, json, jsonutils]
+import custom_async, util, router
+
+type DirInfo = object
+  files: seq[string]
+  folders: seq[string]
+
+proc readDir(path: string): Future[DirInfo] {.async.} =
+  var info = DirInfo()
+  for (kind, item) in walkDir(path, relative=true, skipSpecial=true):
+    if kind == pcFile:
+      info.files.add item
+    elif kind == pcDir:
+      info.folders.add item
+
+  return info
+
+proc callback(req: Request): Future[void] {.async.} =
+  echo "[WS] ", req.reqMethod, " ", req.url
+
+  let headers = newHttpHeaders([("Access-Control-Allow-Origin", "*")])
+
+  withRequest req:
+    options "/":
+      let headers = newHttpHeaders([
+        ("Access-Control-Allow-Origin", "*"),
+        ("Access-Control-Allow-Headers", "authorization"),
+        ("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE"),
+      ])
+      await req.respond(Http204, "", headers)
+
+    get "/info/name":
+      let name = getCurrentDir().splitFile.name
+      await req.respond(Http200, name, headers)
+
+    get "/list/":
+      if path.isAbsolute or path.contains(".."):
+        await req.respond(Http403, "illegal path", headers)
+        break
+
+      echo "list files in ", path
+
+      let result = await readDir(path)
+      let response = result.toJson
+
+      await req.respond(Http200, $response, headers)
+
+    get "/list":
+      echo "list files in ."
+      let result = await readDir(".")
+      let response = result.toJson
+      await req.respond(Http200, $response, headers)
+
+    get "/contents/":
+      if path.isAbsolute or path.contains(".."):
+        await req.respond(Http403, "illegal path", headers)
+        break
+
+      let fullPath = getCurrentDir() / path
+      echo fmt"get content of '{fullPath}'"
+
+      try:
+        var file = openAsync(fullPath, FileMode.fmRead)
+        let content = await file.readAll()
+        file.close()
+
+        await req.respond(Http200, content, headers)
+
+      except CatchableError:
+        await req.respond(Http500, "failed to save: " & getCurrentExceptionMsg(), headers)
+
+    post "/contents/":
+      if path.isAbsolute or path.contains(".."):
+        await req.respond(Http403, "illegal path", headers)
+        break
+
+      let fullPath = getCurrentDir() / path
+      echo fmt"set content of '{fullPath}'"
+
+      try:
+        createDir(fullPath.splitFile.dir)
+
+        var file = openAsync(fullPath, FileMode.fmWrite)
+        await file.write(req.body)
+        file.close()
+
+        await req.respond(Http200, "", headers)
+
+      except CatchableError:
+        await req.respond(Http500, "failed to save: " & getCurrentExceptionMsg(), headers)
+
+    fallback:
+      await req.respond(Http404, "", headers)
+
+proc runWorkspaceServer*(port: Port) {.async.} =
+  var server = newAsyncHttpServer()
+  await server.serve(port, callback)
+
+when isMainModule:
+  const portArg = "--port:"
+  var port = 3000
+  for arg in commandLineParams():
+    if arg.startsWith(portArg):
+      port = arg[portArg.len..^1].parseInt
+    else:
+      echo "Unexpected argument '", arg, "'"
+      quit(1)
+
+  waitFor runWorkspaceServer(Port(port))
