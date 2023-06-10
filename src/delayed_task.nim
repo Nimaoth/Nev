956200553265f038a3a26ff3d21f3e2e1ef7ffc1
Nimaoth <nicolas.otremba@web.de>
2023-06-10 14:11:48 +0200

    added utility for running tasks delayed and improved auto completoin

---
+++ src/delayed_task.nim
@@ -0,0 +1,66 @@
+import std/times
+import custom_async
+
+type
+  DelayedTask* = ref object
+    restartCounter: int
+    active: bool
+    interval: int64
+    nextTick: Time
+    repeat: bool
+    callback: proc()
+
+func isActive*(task: DelayedTask): bool = task.active
+proc reschedule*(task: DelayedTask)
+
+proc tick(task: DelayedTask): Future[void] {.async.} =
+  let restartCounter = task.restartCounter
+
+  defer:
+    if task.restartCounter == restartCounter:
+      task.active = false
+
+  task.active = true
+
+  while true:
+    while true:
+      let now = getTime()
+      if now >= task.nextTick:
+        break
+
+      let timeToNextTick = task.nextTick - now
+      assert timeToNextTick > initDuration()
+      await sleepAsync(timeToNextTick.inMilliseconds.int)
+
+    if task.restartCounter != restartCounter:
+      return
+
+    task.reschedule()
+    task.callback()
+
+    if not task.repeat:
+      break
+
+proc reschedule*(task: DelayedTask) =
+  task.nextTick = getTime() + initDuration(milliseconds=task.interval)
+  if not task.isActive:
+    asyncCheck task.tick()
+
+proc newDelayedTask*(interval: int, repeat: bool, autoActivate: bool, callback: proc()): DelayedTask =
+  result = DelayedTask(interval: interval.int64, repeat: repeat, callback: callback)
+  if autoActivate:
+    result.reschedule()
+
+proc pause*(task: DelayedTask) =
+  task.restartCounter.inc
+  task.active = false
+
+template startDelayed*(interval: int, repeat: bool = false, body: untyped): untyped =
+  newDelayedTask(interval, repeat, true, proc() =
+    body
+  )
+
+template startDelayedPaused*(interval: int, repeat: bool = false, body: untyped): untyped =
+  newDelayedTask(interval, repeat, false, proc() =
+    body
+  )
