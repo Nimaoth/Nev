a039113f8bc4f330a358eb3899cdebe73e44d11c
Nimaoth <nicolas.otremba@web.de>
2023-07-16 15:28:19 +0200

    fixed some performance issues, other improvements

--- src/util.nim
+++ src/util.nim
@@ -8,3 +8,6 @@ template getSome*[T](opt: Option[T], injected: untyped): bool =
 
 template isNotNil*(v: untyped): untyped = not v.isNil
 template toOpenArray*(s: string): auto = s.toOpenArray(0, s.high)
+
+proc `??`*[T: ref object](self: T, els: T): T =
+  return if not self.isNil: self else: els
