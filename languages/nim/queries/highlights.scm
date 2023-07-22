23c3dd178ca85f5f9e8f44ea258d0db3a2a74d5b
Nimaoth <nicolas.otremba@web.de>
2023-07-22 14:23:53 +0200

    improved highlights query

--- languages/nim/queries/highlights.scm
+++ languages/nim/queries/highlights.scm
@@ -2,9 +2,11 @@
  (#match? @support.type.nim "^[A-Z].*$"))
 (typeDesc (primary (symbol (ident) @storage.type)))
 ((ident) @storage.type
- (#match? @storage.type "^(int|int8|int16|int32|int64|uint|uint8|uint16|uint32|uint64|float|float32|float64|bool|string|char)$"))
+ (#match? @storage.type "^(int|int8|int16|int32|int64|uint|uint8|uint16|uint32|uint64|float|float32|float64|bool|string|char|typed|untyped|openArray|seq|array|set)$"))
 ((ident) @keyword
- (#match? @keyword "^(echo|declared|low|high|ord)$"))
+ (#match? @keyword "^(echo|declared|low|high|ord|async|await)$"))
+
+(routine (keyw) (ident (symbol (ident) @variable.function)))
 
 "=" @punctuation
 ":" @punctuation
@@ -30,7 +32,7 @@
   (primarySuffix (functionCall))
 )
 
-;(ident) @variable
+(ident) @variable
 
 [
   (str_lit)
