25610b93633e61c1795048bf8c1918b9b9d8eca0
Nimaoth <nicolas.otremba@web.de>
2024-01-14 02:15:29 +0100

    added more vim keybindings

--- config/default_config.nim
+++ config/default_config.nim
@@ -1,14 +1,6 @@
 import absytree_runtime
 import std/[strutils, sugar, sequtils, macros, json]
 
-import keybindings_vim
-import keybindings_helix
-import keybindings_normal
-
-export keybindings_vim
-export keybindings_helix
-export keybindings_normal
-
 import languages
 
 # Only when compiled to wasm.
@@ -183,10 +175,6 @@ proc loadDefaultKeybindings*(clearExisting: bool = false) =
       setOption("editor.text.line-numbers", LineNumbers.Relative)
       requestRender(true)
 
-    addCommand "editor", "kn", () => loadNormalKeybindings()
-    addCommand "editor", "kv", () => loadVimKeybindings()
-    addCommand "editor", "kh", () => loadHelixKeybindings()
-
     addCommandBlock "editor", "dl": lspLogVerbose(true)
     addCommandBlock "editor", "dL": lspLogVerbose(false)
 
@@ -209,63 +197,61 @@ proc loadDefaultKeybindings*(clearExisting: bool = false) =
   addCommand "popup.selector", "<C-u>", "prev-x"
   addCommand "popup.selector", "<C-d>", "next-x"
 
-  loadNormalKeybindings()
+  # addCommand "editor.text", "<C-SPACE>ts", "reload-treesitter"
 
-  addCommand "editor.text", "<C-SPACE>ts", "reload-treesitter"
+  # setHandleInputs("editor.model", true)
+  # addCommand("editor.model", "<LEFT>", "move-cursor-left-line")
+  # addCommand("editor.model", "<RIGHT>", "move-cursor-right-line")
+  # addCommand("editor.model", "<A-LEFT>", "move-cursor-left")
+  # addCommand("editor.model", "<A-RIGHT>", "move-cursor-right")
+  # addCommand("editor.model", "<UP>", "move-cursor-up")
+  # addCommand("editor.model", "<DOWN>", "move-cursor-down")
+  # addCommand("editor.model", "<A-UP>", "select-node")
+  # addCommand("editor.model", "<A-DOWN>", "move-cursor-down")
+  # addCommand("editor.model", "<C-LEFT>", "move-cursor-left-cell")
+  # addCommand("editor.model", "<C-RIGHT>", "move-cursor-right-cell")
+  # addCommand("editor.model", "<HOME>", "move-cursor-line-start")
+  # addCommand("editor.model", "<END>", "move-cursor-line-end")
+  # addCommand("editor.model", "<A-HOME>", "move-cursor-line-start-inline")
+  # addCommand("editor.model", "<A-END>", "move-cursor-line-end-inline")
 
-  setHandleInputs("editor.model", true)
-  addCommand("editor.model", "<LEFT>", "move-cursor-left-line")
-  addCommand("editor.model", "<RIGHT>", "move-cursor-right-line")
-  addCommand("editor.model", "<A-LEFT>", "move-cursor-left")
-  addCommand("editor.model", "<A-RIGHT>", "move-cursor-right")
-  addCommand("editor.model", "<UP>", "move-cursor-up")
-  addCommand("editor.model", "<DOWN>", "move-cursor-down")
-  addCommand("editor.model", "<A-UP>", "select-node")
-  addCommand("editor.model", "<A-DOWN>", "move-cursor-down")
-  addCommand("editor.model", "<C-LEFT>", "move-cursor-left-cell")
-  addCommand("editor.model", "<C-RIGHT>", "move-cursor-right-cell")
-  addCommand("editor.model", "<HOME>", "move-cursor-line-start")
-  addCommand("editor.model", "<END>", "move-cursor-line-end")
-  addCommand("editor.model", "<A-HOME>", "move-cursor-line-start-inline")
-  addCommand("editor.model", "<A-END>", "move-cursor-line-end-inline")
+  # addCommand("editor.model", "<S-LEFT>", "move-cursor-left-line", true)
+  # addCommand("editor.model", "<S-RIGHT>", "move-cursor-right-line", true)
+  # addCommand("editor.model", "<SA-LEFT>", "move-cursor-left", true)
+  # addCommand("editor.model", "<SA-RIGHT>", "move-cursor-right", true)
+  # addCommand("editor.model", "<S-UP>", "move-cursor-up", true)
+  # addCommand("editor.model", "<S-DOWN>", "move-cursor-down", true)
+  # addCommand("editor.model", "<SA-UP>", "move-cursor-up", true)
+  # addCommand("editor.model", "<SA-DOWN>", "move-cursor-down", true)
+  # addCommand("editor.model", "<SC-LEFT>", "move-cursor-left-cell", true)
+  # addCommand("editor.model", "<SC-RIGHT>", "move-cursor-right-cell", true)
+  # addCommand("editor.model", "<S-HOME>", "move-cursor-line-start", true)
+  # addCommand("editor.model", "<S-END>", "move-cursor-line-end", true)
+  # addCommand("editor.model", "<SA-HOME>", "move-cursor-line-start-inline", true)
+  # addCommand("editor.model", "<SA-END>", "move-cursor-line-end-inline", true)
 
-  addCommand("editor.model", "<S-LEFT>", "move-cursor-left-line", true)
-  addCommand("editor.model", "<S-RIGHT>", "move-cursor-right-line", true)
-  addCommand("editor.model", "<SA-LEFT>", "move-cursor-left", true)
-  addCommand("editor.model", "<SA-RIGHT>", "move-cursor-right", true)
-  addCommand("editor.model", "<S-UP>", "move-cursor-up", true)
-  addCommand("editor.model", "<S-DOWN>", "move-cursor-down", true)
-  addCommand("editor.model", "<SA-UP>", "move-cursor-up", true)
-  addCommand("editor.model", "<SA-DOWN>", "move-cursor-down", true)
-  addCommand("editor.model", "<SC-LEFT>", "move-cursor-left-cell", true)
-  addCommand("editor.model", "<SC-RIGHT>", "move-cursor-right-cell", true)
-  addCommand("editor.model", "<S-HOME>", "move-cursor-line-start", true)
-  addCommand("editor.model", "<S-END>", "move-cursor-line-end", true)
-  addCommand("editor.model", "<SA-HOME>", "move-cursor-line-start-inline", true)
-  addCommand("editor.model", "<SA-END>", "move-cursor-line-end-inline", true)
+  # addCommand("editor.model", "<C-z>", "undo")
+  # addCommand("editor.model", "<C-y>", "redo")
+  # addCommand("editor.model", "<BACKSPACE>", "replace-left")
+  # addCommand("editor.model", "<DELETE>", "replace-right")
+  # addCommand("editor.model", "<SPACE>", "insert-text-at-cursor", " ")
+  # addCommand("editor.model", "<ENTER>", "create-new-node")
+  # addCommand("editor.model", "<TAB>", "select-next-placeholder")
+  # addCommand("editor.model", "<S-TAB>", "select-prev-placeholder")
 
-  addCommand("editor.model", "<C-z>", "undo")
-  addCommand("editor.model", "<C-y>", "redo")
-  addCommand("editor.model", "<BACKSPACE>", "replace-left")
-  addCommand("editor.model", "<DELETE>", "replace-right")
-  addCommand("editor.model", "<SPACE>", "insert-text-at-cursor", " ")
-  addCommand("editor.model", "<ENTER>", "create-new-node")
-  addCommand("editor.model", "<TAB>", "select-next-placeholder")
-  addCommand("editor.model", "<S-TAB>", "select-prev-placeholder")
+  # addCommand("editor.model", "<C-SPACE>", "show-completions")
 
-  addCommand("editor.model", "<C-SPACE>", "show-completions")
+  # addCommand("editor.model", "<LEADER>mr", "run-selected-function")
+  # addCommand("editor.model", "<LEADER>md", "toggle-use-default-cell-builder")
 
-  addCommand("editor.model", "<LEADER>mr", "run-selected-function")
-  addCommand("editor.model", "<LEADER>md", "toggle-use-default-cell-builder")
+  # addCommand("editor.model.completion", "<ENTER>", "finish-edit", true)
+  # addCommand("editor.model.completion", "<ESCAPE>", "hide-completions")
+  # addCommand("editor.model.completion", "<UP>", "select-prev-completion")
+  # addCommand("editor.model.completion", "<DOWN>", "select-next-completion")
+  # addCommand("editor.model.completion", "<C-SPACE>", "move-cursor-start")
+  # addCommand("editor.model.completion", "<TAB>", "apply-selected-completion")
 
-  addCommand("editor.model.completion", "<ENTER>", "finish-edit", true)
-  addCommand("editor.model.completion", "<ESCAPE>", "hide-completions")
-  addCommand("editor.model.completion", "<UP>", "select-prev-completion")
-  addCommand("editor.model.completion", "<DOWN>", "select-next-completion")
-  addCommand("editor.model.completion", "<C-SPACE>", "move-cursor-start")
-  addCommand("editor.model.completion", "<TAB>", "apply-selected-completion")
-
-  addCommand "editor.model.goto", "<END>", "end"
+  # addCommand "editor.model.goto", "<END>", "end"
 
 proc exampleScriptAction*(a: int, b: string): string {.scriptActionWasmNims("example-script-action").} =
   ## Test documentation stuff
