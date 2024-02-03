fc3b7c0e099f03ccd5c3ff39594e97b490b26901
Nimaoth <nicolas.otremba@web.de>
2024-02-03 13:48:35 +0100

    load model keybindings by default

--- config/keybindings_normal.nim
+++ config/keybindings_normal.nim
@@ -106,103 +106,103 @@ proc loadNormalKeybindings*() {.scriptActionWasmNims("load-normal-keybindings").
   addTextCommand "", "gs", "goto-symbol"
   addTextCommand "", "<C-SPACE>", "get-completions"
 
-  block: # model
-    setHandleInputs "editor.model", true
-    setOption "editor.model.cursor.wide.", false
+proc loadModelKeybindings*() {.scriptActionWasmNims("load-model-keybindings").} =
+  setHandleInputs "editor.model", true
+  setOption "editor.model.cursor.wide.", false
 
-    addCommand("editor.model", "<LEFT>", "move-cursor-left-line")
-    addCommand("editor.model", "<RIGHT>", "move-cursor-right-line")
-    addCommand("editor.model", "<UP>", "move-cursor-up")
-    addCommand("editor.model", "<DOWN>", "move-cursor-down")
-    addCommand("editor.model", "<A-LEFT>", "select-prev-neighbor")
-    addCommand("editor.model", "<A-RIGHT>", "select-next-neighbor")
-    addCommand("editor.model", "<SA-LEFT>", "select-prev-neighbor", true)
-    addCommand("editor.model", "<SA-RIGHT>", "select-next-neighbor", true)
-    addCommand("editor.model", "<A-UP>", "select-node")
-    addCommand("editor.model", "<A-DOWN>", "select-prev")
-    addCommand("editor.model", "<C-LEFT>", "move-cursor-left-cell")
-    addCommand("editor.model", "<C-RIGHT>", "move-cursor-right-cell")
-    addCommand("editor.model", "<HOME>", "move-cursor-line-start")
-    addCommand("editor.model", "<END>", "move-cursor-line-end")
-    addCommand("editor.model", "<A-HOME>", "move-cursor-line-start-inline")
-    addCommand("editor.model", "<A-END>", "move-cursor-line-end-inline")
-    addCommand("editor.model", "<C-UP>", "scroll-lines", 1)
-    addCommand("editor.model", "<C-DOWN>", "scroll-lines", -1)
+  addCommand("editor.model", "<LEFT>", "move-cursor-left-line")
+  addCommand("editor.model", "<RIGHT>", "move-cursor-right-line")
+  addCommand("editor.model", "<UP>", "move-cursor-up")
+  addCommand("editor.model", "<DOWN>", "move-cursor-down")
+  addCommand("editor.model", "<A-LEFT>", "select-prev-neighbor")
+  addCommand("editor.model", "<A-RIGHT>", "select-next-neighbor")
+  addCommand("editor.model", "<SA-LEFT>", "select-prev-neighbor", true)
+  addCommand("editor.model", "<SA-RIGHT>", "select-next-neighbor", true)
+  addCommand("editor.model", "<A-UP>", "select-node")
+  addCommand("editor.model", "<A-DOWN>", "select-prev")
+  addCommand("editor.model", "<C-LEFT>", "move-cursor-left-cell")
+  addCommand("editor.model", "<C-RIGHT>", "move-cursor-right-cell")
+  addCommand("editor.model", "<HOME>", "move-cursor-line-start")
+  addCommand("editor.model", "<END>", "move-cursor-line-end")
+  addCommand("editor.model", "<A-HOME>", "move-cursor-line-start-inline")
+  addCommand("editor.model", "<A-END>", "move-cursor-line-end-inline")
+  addCommand("editor.model", "<C-UP>", "scroll-lines", 1)
+  addCommand("editor.model", "<C-DOWN>", "scroll-lines", -1)
 
-    addCommand("editor.model", "<S-LEFT>", "move-cursor-left-line", true)
-    addCommand("editor.model", "<S-RIGHT>", "move-cursor-right-line", true)
-    addCommand("editor.model", "<SA-LEFT>", "move-cursor-left", true)
-    addCommand("editor.model", "<SA-RIGHT>", "move-cursor-right", true)
-    addCommand("editor.model", "<S-UP>", "move-cursor-up", true)
-    addCommand("editor.model", "<S-DOWN>", "move-cursor-down", true)
-    addCommand("editor.model", "<SA-UP>", "move-cursor-up", true)
-    addCommand("editor.model", "<SA-DOWN>", "move-cursor-down", true)
-    addCommand("editor.model", "<SC-LEFT>", "move-cursor-left-cell", true)
-    addCommand("editor.model", "<SC-RIGHT>", "move-cursor-right-cell", true)
-    addCommand("editor.model", "<S-HOME>", "move-cursor-line-start", true)
-    addCommand("editor.model", "<S-END>", "move-cursor-line-end", true)
-    addCommand("editor.model", "<SA-HOME>", "move-cursor-line-start-inline", true)
-    addCommand("editor.model", "<SA-END>", "move-cursor-line-end-inline", true)
+  addCommand("editor.model", "<S-LEFT>", "move-cursor-left-line", true)
+  addCommand("editor.model", "<S-RIGHT>", "move-cursor-right-line", true)
+  addCommand("editor.model", "<SA-LEFT>", "move-cursor-left", true)
+  addCommand("editor.model", "<SA-RIGHT>", "move-cursor-right", true)
+  addCommand("editor.model", "<S-UP>", "move-cursor-up", true)
+  addCommand("editor.model", "<S-DOWN>", "move-cursor-down", true)
+  addCommand("editor.model", "<SA-UP>", "move-cursor-up", true)
+  addCommand("editor.model", "<SA-DOWN>", "move-cursor-down", true)
+  addCommand("editor.model", "<SC-LEFT>", "move-cursor-left-cell", true)
+  addCommand("editor.model", "<SC-RIGHT>", "move-cursor-right-cell", true)
+  addCommand("editor.model", "<S-HOME>", "move-cursor-line-start", true)
+  addCommand("editor.model", "<S-END>", "move-cursor-line-end", true)
+  addCommand("editor.model", "<SA-HOME>", "move-cursor-line-start-inline", true)
+  addCommand("editor.model", "<SA-END>", "move-cursor-line-end-inline", true)
 
-    addCommand("editor.model", "<C-z>", "undo")
-    addCommand("editor.model", "<C-y>", "redo")
+  addCommand("editor.model", "<C-z>", "undo")
+  addCommand("editor.model", "<C-y>", "redo")
 
-    addCommand("editor.model", "<C-c>", "copy-node")
-    addCommand("editor.model", "<C-v>", "paste-node")
+  addCommand("editor.model", "<C-c>", "copy-node")
+  addCommand("editor.model", "<C-v>", "paste-node")
 
-    addCommand("editor.model", "<C-i>", "invert-selection")
-    addModelCommand "", "<C-r>", "select-prev"
-    addModelCommand "", "<C-m>", "select-next"
-    addModelCommand "", "<C-t>", "select-next"
+  addCommand("editor.model", "<C-i>", "invert-selection")
+  addModelCommand "", "<C-r>", "select-prev"
+  addModelCommand "", "<C-m>", "select-next"
+  addModelCommand "", "<C-t>", "select-next"
 
-    addModelCommand "", "<C-g>g", "find-declaration", false
-    addModelCommand "", "<C-g>G", "find-declaration", true
+  addModelCommand "", "<C-g>g", "find-declaration", false
+  addModelCommand "", "<C-g>G", "find-declaration", true
 
-    addModelCommand "", "<*C-g>c", "goto-next-node-of-class", "ConstDecl"
-    addModelCommand "", "<*C-g>C", "goto-prev-node-of-class", "ConstDecl"
-    addModelCommand "", "<*C-g>l", "goto-next-node-of-class", "LetDecl"
-    addModelCommand "", "<*C-g>L", "goto-prev-node-of-class", "LetDecl"
-    addModelCommand "", "<*C-g>v", "goto-next-node-of-class", "VarDecl"
-    addModelCommand "", "<*C-g>V", "goto-prev-node-of-class", "VarDecl"
-    addModelCommand "", "<*C-g>f", "goto-next-node-of-class", "FunctionDefinition"
-    addModelCommand "", "<*C-g>F", "goto-prev-node-of-class", "FunctionDefinition"
-    addModelCommand "", "<*C-g>p", "goto-next-node-of-class", "ParameterDecl"
-    addModelCommand "", "<*C-g>P", "goto-prev-node-of-class", "ParameterDecl"
-    addModelCommand "", "<*C-g>i", "goto-next-node-of-class", "ThenCase"
-    addModelCommand "", "<*C-g>I", "goto-prev-node-of-class", "ThenCase"
-    addModelCommand "", "<*C-g>o", "goto-next-node-of-class", "ForLoop"
-    addModelCommand "", "<*C-g>O", "goto-prev-node-of-class", "ForLoop"
-    addModelCommand "", "<*C-g>w", "goto-next-node-of-class", "WhileExpression"
-    addModelCommand "", "<*C-g>W", "goto-prev-node-of-class", "WhileExpression"
-    addModelCommand "", "<*C-g>r", "goto-next-reference"
-    addModelCommand "", "<*C-g>R", "goto-prev-reference"
-    addModelCommand "", "<*C-g>e", "goto-next-invalid-node"
-    addModelCommand "", "<*C-g>E", "goto-prev-invalid-node"
+  addModelCommand "", "<*C-g>c", "goto-next-node-of-class", "ConstDecl"
+  addModelCommand "", "<*C-g>C", "goto-prev-node-of-class", "ConstDecl"
+  addModelCommand "", "<*C-g>l", "goto-next-node-of-class", "LetDecl"
+  addModelCommand "", "<*C-g>L", "goto-prev-node-of-class", "LetDecl"
+  addModelCommand "", "<*C-g>v", "goto-next-node-of-class", "VarDecl"
+  addModelCommand "", "<*C-g>V", "goto-prev-node-of-class", "VarDecl"
+  addModelCommand "", "<*C-g>f", "goto-next-node-of-class", "FunctionDefinition"
+  addModelCommand "", "<*C-g>F", "goto-prev-node-of-class", "FunctionDefinition"
+  addModelCommand "", "<*C-g>p", "goto-next-node-of-class", "ParameterDecl"
+  addModelCommand "", "<*C-g>P", "goto-prev-node-of-class", "ParameterDecl"
+  addModelCommand "", "<*C-g>i", "goto-next-node-of-class", "ThenCase"
+  addModelCommand "", "<*C-g>I", "goto-prev-node-of-class", "ThenCase"
+  addModelCommand "", "<*C-g>o", "goto-next-node-of-class", "ForLoop"
+  addModelCommand "", "<*C-g>O", "goto-prev-node-of-class", "ForLoop"
+  addModelCommand "", "<*C-g>w", "goto-next-node-of-class", "WhileExpression"
+  addModelCommand "", "<*C-g>W", "goto-prev-node-of-class", "WhileExpression"
+  addModelCommand "", "<*C-g>r", "goto-next-reference"
+  addModelCommand "", "<*C-g>R", "goto-prev-reference"
+  addModelCommand "", "<*C-g>e", "goto-next-invalid-node"
+  addModelCommand "", "<*C-g>E", "goto-prev-invalid-node"
 
-    addCommand "editor.model", "<BACKSPACE>", "replace-left"
-    addCommand "editor.model", "<DELETE>", "replace-right"
-    addCommand "editor.model", "<C-BACKSPACE>", "delete-left"
-    addCommand "editor.model", "<C-DELETE>", "delete-right"
-    addCommand "editor.model", "<ENTER>", "create-new-node"
-    addCommand "editor.model", "<TAB>", "select-next-placeholder"
-    addCommand "editor.model", "<S-TAB>", "select-prev-placeholder"
+  addCommand "editor.model", "<BACKSPACE>", "replace-left"
+  addCommand "editor.model", "<DELETE>", "replace-right"
+  addCommand "editor.model", "<C-BACKSPACE>", "delete-left"
+  addCommand "editor.model", "<C-DELETE>", "delete-right"
+  addCommand "editor.model", "<ENTER>", "create-new-node"
+  addCommand "editor.model", "<TAB>", "select-next-placeholder"
+  addCommand "editor.model", "<S-TAB>", "select-prev-placeholder"
 
-    addCommand "editor.model", "<C-SPACE>", "show-completions"
+  addCommand "editor.model", "<C-SPACE>", "show-completions"
 
-    addCommand "editor.model", "<LEADER>mr", "run-selected-function"
-    addCommand "editor.model", "<LEADER>md", "toggle-use-default-cell-builder"
-    addCommand "editor.model", "<LEADER>mc", "compile-language"
+  addCommand "editor.model", "<LEADER>mr", "run-selected-function"
+  addCommand "editor.model", "<LEADER>md", "toggle-use-default-cell-builder"
+  addCommand "editor.model", "<LEADER>mc", "compile-language"
 
-    addCommand "editor.model", "gd", "goto-definition"
-    addCommand "editor.model", "gp", "goto-prev-reference"
-    addCommand "editor.model", "gn", "goto-next-reference"
-    addCommand "editor.model", "tt", "toggle-bool-cell"
+  addCommand "editor.model", "gd", "goto-definition"
+  addCommand "editor.model", "gp", "goto-prev-reference"
+  addCommand "editor.model", "gn", "goto-next-reference"
+  addCommand "editor.model", "tt", "toggle-bool-cell"
 
-    addCommand "editor.model.completion", "<ESCAPE>", "hide-completions"
-    addCommand "editor.model.completion", "<UP>", "select-prev-completion"
-    addCommand "editor.model.completion", "<DOWN>", "select-next-completion"
-    addCommand "editor.model.completion", "<C-SPACE>", "move-cursor-start"
-    addCommand "editor.model.completion", "<TAB>", "apply-selected-completion"
-    addCommand "editor.model.completion", "<ENTER>", "apply-selected-completion"
+  addCommand "editor.model.completion", "<ESCAPE>", "hide-completions"
+  addCommand "editor.model.completion", "<UP>", "select-prev-completion"
+  addCommand "editor.model.completion", "<DOWN>", "select-next-completion"
+  addCommand "editor.model.completion", "<C-SPACE>", "move-cursor-start"
+  addCommand "editor.model.completion", "<TAB>", "apply-selected-completion"
+  addCommand "editor.model.completion", "<ENTER>", "apply-selected-completion"
 
-    addCommand "editor.model.goto", "<END>", "end"
+  addCommand "editor.model.goto", "<END>", "end"
