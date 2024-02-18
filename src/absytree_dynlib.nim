9c24fe5bf652149915dd9f150f8493d09fae2e1b
Nimaoth <nicolas.otremba@web.de>
2024-02-18 17:39:13 +0100

    started working on new platform for embedding in unreal

---
+++ src/absytree_dynlib.nim
@@ -0,0 +1,191 @@
+when defined(js):
+  {.error: "absytree_terminal.nim does not work in js backend. Use absytree_js.nim instead.".}
+
+when defined(windows):
+  static:
+    echo "Compiling for windows"
+elif defined(linux):
+  static:
+    echo "Compiling for linux"
+elif defined(posix):
+  static:
+    echo "Compiling for posix"
+else:
+  static:
+    echo "Compiling for unknown"
+
+import std/[parseopt, options, macros]
+import misc/[custom_logger]
+import compilation_config, scripting_api, app_options
+
+type UeLog = proc(level: int32, message: cstring) {.cdecl.}
+
+logCategory "main"
+
+logger.enableFileLogger()
+
+type ExternLogger = ref object of Logger
+  discard
+
+var ueLogFn: UeLog = nil
+method log*(logger: ExternLogger, level: Level, args: varargs[string, `$`]) =
+  if not ueLogFn.isNil:
+    let ueLevel: int32 = level.int32
+    let ln = substituteLog("", level, args)
+    {.gcsafe.}:
+      ueLogFn(ueLevel, ln.cstring)
+  else:
+    let ln = substituteLog(defaultFmtStr, level, args)
+    echo ln
+
+var externLogger = ExternLogger()
+logger.setConsoleLogger(externLogger)
+
+import std/[strformat]
+import vmath
+import misc/[util, timer, custom_async]
+import platform/platform
+import ui/widget_builders
+import text/language/language_server
+import app
+
+import platform/[platform_externc, filesystem]
+
+# Do this after every import
+# Don't remove those imports, they are needed by createNimScriptContextConstructorAndGenerateBindings
+import std/[macrocache]
+import ast/model_document
+import text/text_editor
+import text/language/lsp_client
+import selector_popup
+import wasm3, wasm3/[wasm3c, wasmconversions]
+import ui/node
+import clipboard
+
+createNimScriptContextConstructorAndGenerateBindings()
+
+type OnAppCreated = proc(app: ptr AppObject, userData: pointer) {.cdecl.}
+
+var rend: ExternCPlatform = nil
+var gApp: App = nil
+
+proc runApp(onAppCreated: OnAppCreated, userData: pointer): Future[void] {.async.} =
+  echo "Creating platform..."
+  rend = new ExternCPlatform
+  rend.init()
+
+  echo "Creating app..."
+  var opts = AppOptions(disableNimScriptPlugins: true)
+  gApp = await newEditor(Backend.Gui, rend, opts)
+  onAppCreated(gApp[].addr, userData)
+
+proc shutdown() =
+  destroyClipboard()
+
+  if gApp.isNotNil:
+    log lvlInfo, "Shutting down editor"
+    gApp.shutdown()
+
+  if rend.isNotNil:
+    log lvlInfo, "Shutting down platform"
+    rend.deinit()
+
+proc NimMain() {.cdecl, importc.}
+
+proc absytree_init(userData: pointer, onAppCreated: OnAppCreated, ueLog: UeLog) {.exportc, dynlib, cdecl.} =
+  NimMain()
+  ueLogFn = ueLog
+  echo "Initializing Absytree..."
+  fs.init("D:/Absytree")
+  waitFor runApp(onAppCreated, userData)
+
+proc absytree_poll(timeoutMs: int32) {.exportc, dynlib, cdecl.} =
+  try:
+    poll(timeoutMs.int)
+  except ValueError:
+    discard
+
+proc absytree_shutdown() {.exportc, dynlib, cdecl.} =
+  echo "Shutting down Absytree..."
+  shutdown()
+  GC_FullCollect()
+
+proc absytree_input_keys*(self: ptr AppObject, input: cstring) {.exportc, dynlib, cdecl.} =
+  echo cast[int](gApp[].addr), ", ", cast[int](self.pointer)
+  if gApp.isNotNil:
+    gApp.inputKeys($input)
+  # self[].inputKeys($input)
+
+proc absytree_key_down*(input: int64, mods: int32): bool {.exportc, dynlib, cdecl.} =
+  if gApp.isNotNil:
+    let mods = cast[Modifiers](mods)
+    debugf"Key down: {input}, {mods}"
+    gApp.handleKeyPress(input, mods)
+    return true
+
+proc absytree_key_up*(input: int64, mods: int32): bool {.exportc, dynlib, cdecl.} =
+  if gApp.isNotNil:
+    let mods = cast[Modifiers](mods)
+    debugf"Key up: {input}, {mods}"
+    gApp.handleKeyRelease(input, mods)
+    return true
+
+proc absytree_mouse_down*(input: int64, mods: int32, x, y: float32): bool {.exportc, dynlib, cdecl.} =
+  if gApp.isNotNil:
+    let mods = cast[Modifiers](mods)
+    debugf"mouse down: {input}, {mods}, {x}, {y}"
+    gApp.handleMousePress(input.MouseButton, mods, vec2(x, y))
+    return true
+
+proc absytree_mouse_up*(input: int64, mods: int32, x, y: float32): bool {.exportc, dynlib, cdecl.} =
+  if gApp.isNotNil:
+    let mods = cast[Modifiers](mods)
+    debugf"mouse up: {input}, {mods}, {x}, {y}"
+    gApp.handleMouseRelease(input.MouseButton, mods, vec2(x, y))
+    return true
+
+proc absytree_mouse_scroll*(input: int64, mods: int32, x, y: float32): bool {.exportc, dynlib, cdecl.} =
+  if gApp.isNotNil:
+    let mods = cast[Modifiers](mods)
+    debugf"mouse scroll: {input}, {mods}, {x}, {y}"
+    gApp.handleScroll(vec2(0, input.float32), vec2(x, y), mods)
+    return true
+
+proc absytree_render*(width: float32, height: float32) {.exportc, dynlib, cdecl.} =
+  if gApp.isNil:
+    return
+
+  var updateTime, renderTime: float
+  block:
+    let delta = gApp.frameTimer.elapsed.ms
+    gApp.frameTimer = startTimer()
+
+    let updateTimer = startTimer()
+
+    rend.builder.frameTime = delta
+
+    rend.size = vec2(width, height)
+    let size = rend.size
+
+    var rerender = false
+    if size != rend.builder.root.boundsActual.wh or rend.requestedRender:
+      rend.requestedRender = false
+      rend.builder.beginFrame(size)
+      gApp.updateWidgetTree(rend.builder.frameIndex)
+      rend.builder.endFrame()
+      rerender = true
+    elif rend.builder.animatingNodes.len > 0:
+      rend.builder.frameIndex.inc
+      rend.builder.postProcessNodes()
+      rerender = true
+
+    updateTime = updateTimer.elapsed.ms
+
+    let renderTimer = startTimer()
+    rend.render()
+    renderTime = renderTimer.elapsed.ms
+
+    let frameTime = gApp.frameTimer.elapsed.ms
+    if rerender and frameTime > 5:
+      debugf"Update: {updateTime}ms, Render: {renderTime}ms, Frame: {frameTime}ms"
+
---
+++ src/platform/platform_externc.nim
@@ -0,0 +1,219 @@
+import std/[tables, strutils, options, sets]
+import chroma, vmath, pixie/[contexts, fonts]
+import misc/[custom_logger, util, event, id, rect_utils]
+import ui/node
+import platform, platform/filesystem
+import input, theme
+
+export platform
+
+logCategory "gui-platform"
+
+type
+  ExternCPlatform* = ref object of Platform
+    ctx*: Context
+    currentModifiers: Modifiers
+    currentMouseButtons: set[MouseButton]
+    eventCounter: int
+
+    fontRegular: string
+    fontBold*: string
+    fontItalic*: string
+    fontBoldItalic*: string
+
+    mSize: Vec2
+    lastSize: Vec2
+    renderedSomethingLastFrame: bool
+
+    lastFontSize: float
+    mLineHeight: float
+    mLineDistance: float = 2
+    mCharWidth: float
+    mCharGap: float
+
+    typefaces: Table[string, Typeface]
+
+    drawnNodes: seq[UINode]
+
+proc getFont*(self: ExternCPlatform, font: string, fontSize: float32): Font
+proc getFont*(self: ExternCPlatform, fontSize: float32, style: set[FontStyle]): Font
+proc getFont*(self: ExternCPlatform, fontSize: float32, flags: UINodeFlags): Font
+
+method init*(self: ExternCPlatform) =
+  self.supportsThinCursor = true
+
+  self.builder = newNodeBuilder()
+  self.builder.useInvalidation = true
+
+  self.ctx = newContext(1, 1)
+  self.ctx.fillStyle = rgb(255, 255, 255)
+  self.ctx.strokeStyle = rgb(255, 255, 255)
+  self.ctx.font = "fonts/DejaVuSansMono.ttf"
+  self.ctx.textBaseline = TopBaseline
+
+  self.fontRegular = "fonts/DejaVuSansMono.ttf"
+  self.fontBold = "fonts/DejaVuSansMono-Bold.ttf"
+  self.fontItalic = "fonts/DejaVuSansMono-Oblique.ttf"
+  self.fontBoldItalic = "fonts/DejaVuSansMono-BoldOblique.ttf"
+
+  # This sets the font size of self.ctx and recalculates the char width
+  self.fontSize = 16
+
+  self.layoutOptions.getTextBounds = proc(text: string, fontSizeIncreasePercent: float = 0): Vec2 =
+    let font = self.getFont(self.ctx.font, self.ctx.fontSize * (1 + fontSizeIncreasePercent))
+    if text.len == 0:
+      let arrangement = font.typeset(" ")
+      result = vec2(0, arrangement.layoutBounds().y)
+    else:
+      let arrangement = font.typeset(text)
+      result = arrangement.layoutBounds()
+
+method deinit*(self: ExternCPlatform) =
+  discard
+
+method requestRender*(self: ExternCPlatform, redrawEverything = false) =
+  self.requestedRender = true
+  self.redrawEverything = self.redrawEverything or redrawEverything
+
+proc getFont*(self: ExternCPlatform, font: string, fontSize: float32): Font =
+  if font == "":
+    # todo
+    discard
+
+  if font notin self.typefaces:
+    self.typefaces[font] = readTypeface(fs.getApplicationFilePath(font))
+
+  result = newFont(self.typefaces.getOrDefault(font, nil))
+  result.paint.color = color(1, 1, 1)
+  result.size = fontSize
+
+proc getFont*(self: ExternCPlatform, fontSize: float32, style: set[FontStyle]): Font =
+  if Italic in style and Bold in style:
+    return self.getFont(self.fontBoldItalic, fontSize)
+  if Italic in style:
+    return self.getFont(self.fontItalic, fontSize)
+  if Bold in style:
+    return self.getFont(self.fontBold, fontSize)
+  return self.getFont(self.fontRegular, fontSize)
+
+proc getFont*(self: ExternCPlatform, fontSize: float32, flags: UINodeFlags): Font =
+  if TextItalic in flags and TextBold in flags:
+    return self.getFont(self.fontBoldItalic, fontSize)
+  if TextItalic in flags:
+    return self.getFont(self.fontItalic, fontSize)
+  if TextBold in flags:
+    return self.getFont(self.fontBold, fontSize)
+  return self.getFont(self.fontRegular, fontSize)
+
+method size*(self: ExternCPlatform): Vec2 = self.mSize
+proc `size=`*(self: ExternCPlatform, size: Vec2) = self.mSize = size
+
+method sizeChanged*(self: ExternCPlatform): bool =
+  let s = self.size
+  return s.x != self.lastSize.x or s.y != self.lastSize.y
+
+proc updateCharWidth*(self: ExternCPlatform) =
+  let font = self.getFont(self.ctx.font, self.ctx.fontSize)
+  let bounds = font.typeset(repeat("#", 100)).layoutBounds()
+  let boundsSingle = font.typeset("#").layoutBounds()
+  self.mCharWidth = bounds.x / 100
+  self.mCharGap = (bounds.x / 100) - boundsSingle.x
+  self.mLineHeight = bounds.y
+
+  self.builder.charWidth = bounds.x / 100.0
+  self.builder.lineHeight = bounds.y - 3
+  self.builder.lineGap = self.mLineDistance
+
+method `fontSize=`*(self: ExternCPlatform, fontSize: float) =
+  self.ctx.fontSize = fontSize
+  self.updateCharWidth()
+
+method `lineDistance=`*(self: ExternCPlatform, lineDistance: float) = self.mLineDistance = lineDistance
+method fontSize*(self: ExternCPlatform): float = self.ctx.fontSize
+method lineDistance*(self: ExternCPlatform): float = self.mLineDistance
+method lineHeight*(self: ExternCPlatform): float = self.mLineHeight
+method charWidth*(self: ExternCPlatform): float = self.mCharWidth
+method charGap*(self: ExternCPlatform): float = self.mCharGap
+
+method measureText*(self: ExternCPlatform, text: string): Vec2 = self.getFont(self.ctx.font, self.ctx.fontSize).typeset(text).layoutBounds()
+
+method processEvents*(self: ExternCPlatform): int =
+  self.eventCounter = 0
+  return self.eventCounter
+
+proc drawNode(builder: UINodeBuilder, platform: ExternCPlatform, node: UINode, offset: Vec2 = vec2(0, 0), force: bool = false)
+
+proc randomColor(node: UINode, a: float32): Color =
+  let h = node.id.hash
+  result.r = (((h shr 0) and 0xff).float32 / 255.0).sqrt
+  result.g = (((h shr 8) and 0xff).float32 / 255.0).sqrt
+  result.b = (((h shr 16) and 0xff).float32 / 255.0).sqrt
+  result.a = a
+
+proc UnrealDrawRect*(x, y, width, height, r, g, b, a: float32) {.importc, dynlib: "UnrealEditor-AbsytreeUE-Win64-DebugGame.dll", cdecl.}
+proc UnrealDrawText*(x, y, r, g, b, a: float32, text: cstring) {.importc, dynlib: "UnrealEditor-AbsytreeUE-Win64-DebugGame.dll", cdecl.}
+
+method render*(self: ExternCPlatform) =
+  # Clear the screen and begin a new frame.
+  # debugf"render: font size: {self.ctx.fontSize}, char width: {self.mCharWidth}, line height: {self.mLineHeight}, line distance: {self.mLineDistance}"
+
+  if self.ctx.fontSize != self.lastFontSize:
+    self.lastFontSize = self.ctx.fontSize
+
+  self.redrawEverything = true
+
+  self.drawnNodes.setLen 0
+
+  var renderedSomething = true
+  self.builder.drawNode(self, self.builder.root, force = self.redrawEverything)
+
+  self.renderedSomethingLastFrame = renderedSomething;
+  self.redrawEverything = false
+  self.lastSize = self.size
+
+proc drawNode(builder: UINodeBuilder, platform: ExternCPlatform, node: UINode, offset: Vec2 = vec2(0, 0), force: bool = false) =
+  var nodePos = offset
+  nodePos.x += node.boundsActual.x
+  nodePos.y += node.boundsActual.y
+
+  var force = force
+
+  if builder.useInvalidation and not force and node.lastChange < builder.frameIndex:
+    return
+
+  node.lastRenderTime = builder.frameIndex
+
+  # if node.flags.any &{UINodeFlag.FillBackground, DrawBorder, DrawText}:
+  #   platform.drawnNodes.add node
+
+  if node.flags.any &{UINodeFlag.FillBackground, DrawText}:
+    force = true
+
+  node.lx = nodePos.x
+  node.ly = nodePos.y
+  node.lw = node.boundsActual.w
+  node.lh = node.boundsActual.h
+  let bounds = rect(nodePos.x, nodePos.y, node.boundsActual.w, node.boundsActual.h)
+
+  if FillBackground in node.flags:
+    UnrealDrawRect(bounds.x, bounds.y, bounds.w, bounds.h, node.backgroundColor.r, node.backgroundColor.g, node.backgroundColor.b, node.backgroundColor.a)
+
+  # # Mask the rest of the rendering is this function to the contentBounds
+  # if MaskContent in node.flags:
+  #   platform.boxy.pushLayer()
+  # defer:
+  #   if MaskContent in node.flags:
+  #     platform.boxy.pushLayer()
+  #     platform.boxy.drawRect(bounds, color(1, 0, 0, 1))
+  #     platform.boxy.popLayer(blendMode = MaskBlend)
+  #     platform.boxy.popLayer()
+
+  if DrawText in node.flags:
+    # todo: flags
+    UnrealDrawText(nodePos.x.floor, nodePos.y.floor, node.textColor.r, node.textColor.g, node.textColor.b, node.textColor.a, node.text.cstring)
+
+  for _, c in node.children:
+    builder.drawNode(platform, c, nodePos, force)
+
+  # if DrawBorder in node.flags:
+  #   platform.boxy.strokeRect(bounds, node.borderColor)
