29a3ce75340cfc21100db71507a868627bdfc90e
Nimaoth <nicolas.otremba@web.de>
2024-02-18 17:47:06 +0100

    disable system clipboard for dll

--- src/compilation_config.nim
+++ src/compilation_config.nim
@@ -3,6 +3,7 @@ const exposeScriptingApi* {.booldefine.}: bool = false
 const enableGui* {.booldefine.}: bool = false
 const enableTerminal* {.booldefine.}: bool = false
 const enableTableIdCacheChecking* {.booldefine.}: bool = false
+const enableSystemClipboard* {.booldefine.}: bool = true
 
 when not defined(js):
   when not enableGui and not enableTerminal:
