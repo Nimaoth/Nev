e23d5228fa8ce8a8559a58d5ba98f87cf1ec6e94
Nimaoth <nicolas.otremba@web.de>
2024-03-16 18:19:16 +0100

    updated nim highlight queries for new nim treesitter vesion

--- languages/nim/queries/highlights.scm
+++ languages/nim/queries/highlights.scm
@@ -1,12 +1,12 @@
 ((ident) @support.type.nim
  (#match? @support.type.nim "^[A-Z].*$"))
-(typeDesc (primary (symbol (ident) @storage.type)))
+(typeDesc (primaryTypeDesc (symbol (ident) @storage.type)))
 ((ident) @storage.type
  (#match? @storage.type "^(int|int8|int16|int32|int64|uint|uint8|uint16|uint32|uint64|float|float32|float64|bool|string|char|typed|untyped|openArray|seq|array|set)$"))
 ((ident) @keyword
  (#match? @keyword "^(echo|declared|low|high|ord|async|await)$"))
 
-(routine (keyw) (ident (symbol (ident) @variable.function)))
+(routine (keyw) (symbol (ident) @variable.function))
 
 "=" @punctuation
 ":" @punctuation
