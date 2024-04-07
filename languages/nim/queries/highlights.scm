6bf4b1013ba66835c157b2360da51f94f0c41a3b
Nimaoth <nicolas.otremba@web.de>
2024-04-07 15:12:26 +0200

    fixed syntax highlighting for more nim keywords

--- languages/nim/queries/highlights.scm
+++ languages/nim/queries/highlights.scm
@@ -4,7 +4,7 @@
 ((ident) @storage.type
  (#match? @storage.type "^(int|int8|int16|int32|int64|uint|uint8|uint16|uint32|uint64|float|float32|float64|bool|string|char|typed|untyped|openArray|seq|array|set)$"))
 ((ident) @keyword
- (#match? @keyword "^(echo|declared|low|high|ord|async|await|if|else|elif|some|none|void|return)$"))
+ (#match? @keyword "^(echo|declared|low|high|ord|async|await|if|else|elif|some|none|void|return|for|try|except|when|let|var|const)$"))
 
 (routine (keyw) (symbol (ident) @variable.function))
 
