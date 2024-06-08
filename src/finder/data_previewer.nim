bf0feb219666ff511487728e898f438343625749
Nimaoth <nicolas.otremba@web.de>
2024-06-08 18:06:10 +0200

    Added selector popup for launch configurations

---
+++ src/finder/data_previewer.nim
@@ -0,0 +1,48 @@
+import std/[options, strformat]
+import misc/[util, custom_logger]
+import text/[text_editor, text_document]
+import scripting_api except DocumentEditor, TextDocumentEditor, AstDocumentEditor
+import finder, previewer
+import config_provider
+
+logCategory "data-previewer"
+
+type
+  DataPreviewer* = ref object of Previewer
+    editor: TextDocumentEditor
+    tempDocument: TextDocument
+    getPreviewTextImpl: proc(item: FinderItem): string
+
+proc newDataPreviewer*(configProvider: ConfigProvider, language = string.none,
+    getPreviewTextImpl: proc(item: FinderItem): string = nil): DataPreviewer =
+
+  new result
+
+  result.tempDocument = newTextDocument(configProvider, language=language,
+    createLanguageServer=false)
+  result.tempDocument.readOnly = true
+  result.getPreviewTextImpl = getPreviewTextImpl
+
+method deinit*(self: DataPreviewer) =
+  logScope lvlInfo, &"[deinit] Destroying data file previewer"
+  if self.tempDocument.isNotNil:
+    self.tempDocument.deinit()
+
+  self[] = default(typeof(self[]))
+
+method delayPreview*(self: DataPreviewer) =
+  discard
+
+method previewItem*(self: DataPreviewer, item: FinderItem, editor: DocumentEditor) =
+  if not (editor of TextDocumentEditor):
+    return
+
+  self.editor = editor.TextDocumentEditor
+  self.editor.setDocument(self.tempDocument)
+  self.editor.selection = (0, 0).toSelection
+  self.editor.scrollToTop()
+
+  if self.getPreviewTextImpl.isNotNil:
+    self.tempDocument.content = self.getPreviewTextImpl(item)
+  else:
+    self.tempDocument.content = item.data
