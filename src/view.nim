106393c7263f4e276081babed8654c9ea8c11b4b
Nimaoth <nicolas.otremba@web.de>
2024-06-15 22:49:04 +0200

    Support getActiveEditor for debugger output editor

--- src/view.nim
+++ src/view.nim
@@ -1,5 +1,5 @@
-import std/tables
-import events
+import std/[options, tables]
+import events, document_editor
 
 type
   View* = ref object of RootObj
@@ -18,4 +18,7 @@ method markDirty*(view: View, notify: bool = true) {.base.} =
   view.dirty = true
 
 method getEventHandlers*(self: View, inject: Table[string, EventHandler]): seq[EventHandler] {.base.} =
+  discard
+
+method getActiveEditor*(self: View): Option[DocumentEditor] {.base.} =
   discard
