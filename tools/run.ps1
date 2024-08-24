63e89dcdc13d49bcde194abfa095d7f39e39ef65
Nimaoth <nicolas.otremba@web.de>
2024-08-24 13:09:45 +0200

    Added script for running and auto restarting the editor

---
+++ tools/run.ps1
@@ -0,0 +1,12 @@
+$exitCode = 123
+while ($exitCode -eq 123) {
+    Write-Host "Copy ast.exe to ast-temp.exe"
+    &cp ast.exe ast-temp.exe
+    Write-Host "Launch ast-temp.exe"
+    $process = Start-Process -FilePath "ast-temp.exe" -NoNewWindow -PassThru
+    Wait-Process -Id $process.Id
+    # For some reason $process.ExitCode returns nothing, but the reflection access works, BUT only if we use $process.ExitCode first???
+    $exitCode = $process.ExitCode
+    $exitCode = $process.GetType().GetField('exitCode', 'NonPublic, Instance').GetValue($process)
+    Write-Host "Exited with code $exitCode"
+}
