95723d20120f2b275f0d0d20e5bf73ebf39efb83
Nimaoth <nicolas.otremba@web.de>
2024-10-06 19:00:09 +0200

    Deleted a bunch of unused stuff

--- tools/cloc.sh
+++ tools/cloc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cloc --by-file --include-lang=Nim --exclude-list-file=cloc-exclude-list-file.txt --exclude-dir=nimcache,fonts,temp,int,themes .
+cloc --by-file --include-lang=Nim --exclude-list-file=tools/cloc-exclude-list-file.txt --exclude-dir=nimcache,fonts,temp,int,themes .
