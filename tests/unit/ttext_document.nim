97a213b5457d55521a236b8dcc81e8dcc2b61c51
Nimaoth <nicolas.otremba@web.de>
2025-06-21 20:52:40 +0200

    Fixed : in vim mode not working when no file is open, fixed some tests

--- tests/unit/ttext_document.nim
+++ tests/unit/ttext_document.nim
@@ -7,33 +7,44 @@ discard """
 """
 
 import std/[unittest, options, json, sequtils]
-import misc/[util, traits]
+import misc/[util, traits, custom_logger]
 import text/text_document
-import config_provider, scripting_api
+import config_provider, scripting_api, service
+import platform/platform, platform_service
+
+defineSetAllDefaultSettings()
+
+type
+  NilPlatform* = ref object of Platform
+
+gServices = Services()
+gServices.addBuiltinServices()
+gServices.getService(PlatformService).get.setPlatform(NilPlatform())
+gServices.waitForServices()
 
 suite "Text Document":
 
   test "create document":
-    let document = newTextDocument("", "abc", false)
+    let document = newTextDocument(gServices, "", "abc")
     check document.isNotNil
     check document.contentString == "abc"
 
   test "insert text":
-    let document = newTextDocument("", "abc", false)
+    let document = newTextDocument(gServices, "", "abc")
     check document.contentString == "abc"
 
     let selection = (0, 3).toSelection
-    let newSelections = document.insert([selection], [selection], ["def"], false, false)
+    let newSelections = document.edit([selection], [selection], ["def"])
 
     check newSelections == [((0, 3), (0, 6))]
     check document.contentString == "abcdef"
 
   test "delete text":
-    let document = newTextDocument("", "abcdef", false)
+    let document = newTextDocument(gServices, "", "abcdef")
     check document.contentString == "abcdef"
 
     let selection = ((0, 2), (0, 5))
-    let newSelections = document.delete([selection], [selection], false, false)
+    let newSelections = document.edit([selection], [selection], [""])
 
     check newSelections == [(0, 2).toSelection]
     check document.contentString == "abf"
