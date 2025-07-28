42a9bcf16aa92cfac8c30e654dd86a271ef43309
Nimaoth <nicolas.otremba@web.de>
2025-07-28 15:44:14 +0200

    Updated some tools

--- tools/run.ps1
+++ tools/run.ps1
@@ -5,7 +5,8 @@ while ($exitCode -eq 123) {
     Write-Host "Copy nev.exe to nev-temp.exe"
     &cp nev.exe nev-temp.exe
     $argString = $args -join " "
-    $argString = "-p:editor.prompt-before-quit=true $argString"
+    # $argString = "-p:editor.prompt-before-quit=true -p:editor.watch-app-config=false -p:editor.watch-user-config=false -p:editor.watch-workspace-config=false $argString"
+    $argString = "-p:editor.prompt-before-quit=true -p:editor.watch-app-config=true -p:editor.watch-user-config=false -p:editor.watch-workspace-config=false $argString"
     Write-Host "Launch nev-temp.exe $argString"
     if ($argString -eq "") {
         $process = Start-Process -FilePath "nev-temp.exe" -NoNewWindow -PassThru
