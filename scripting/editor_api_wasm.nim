19c80db25238dde3536cd82f84cecc85d1e30f6c
jfaz <56184947+jfaz1@users.noreply.github.com>
2025-08-16 05:01:12 -0400

    Add opening session in-place (#66)
    * Add opening session in-place
    
    * Update open-session/explore-files commands with new window option

--- scripting/editor_api_wasm.nim
+++ scripting/editor_api_wasm.nim
@@ -292,19 +292,21 @@ proc loadTheme*(name: string; force: bool = false) {.gcsafe, raises: [].} =
       argsJsonString.cstring)
 
 
-proc editor_openSession_void_App_string_bool_float_float_float_wasm(arg: cstring): cstring {.
-    importc.}
-proc openSession*(root: string = "home://"; preview: bool = true;
-                  scaleX: float = 0.9; scaleY: float = 0.8;
-                  previewScale: float = 0.4) {.gcsafe, raises: [].} =
+proc editor_openSession_void_App_bool_string_bool_float_float_float_wasm(
+    arg: cstring): cstring {.importc.}
+proc openSession*(newWindow: bool = false; root: string = "home://";
+                  preview: bool = true; scaleX: float = 0.9;
+                  scaleY: float = 0.8; previewScale: float = 0.4) {.gcsafe,
+    raises: [].} =
   var argsJson = newJArray()
+  argsJson.add newWindow.toJson()
   argsJson.add root.toJson()
   argsJson.add preview.toJson()
   argsJson.add scaleX.toJson()
   argsJson.add scaleY.toJson()
   argsJson.add previewScale.toJson()
   let argsJsonString = $argsJson
-  let res {.used.} = editor_openSession_void_App_string_bool_float_float_float_wasm(
+  let res {.used.} = editor_openSession_void_App_bool_string_bool_float_float_float_wasm(
       argsJsonString.cstring)
 
 
