92376261a15ab7d249c505721e3f4b837c0284f0
Nimaoth <nicolas.otremba@web.de>
2026-02-08 15:28:49 +0100

    Move hover commands to hover component

--- src/module_imports.nim
+++ src/module_imports.nim
@@ -3,6 +3,7 @@ import "../modules/hover_component.nim"
 import "../modules/language_server_ctags.nim"
 import "../modules/language_server_regex.nim"
 import "../modules/workspace_edit.nim"
+import "../modules/command_component.nim"
 import "../modules/terminal/terminal.nim"
 import "../modules/language_server_lsp/language_server_lsp.nim"
 import "../modules/language_server_paths.nim"
@@ -15,6 +16,7 @@ proc initModules*() =
   init_module_language_server_ctags()
   init_module_language_server_regex()
   init_module_workspace_edit()
+  init_module_command_component()
   init_module_terminal()
   init_module_language_server_lsp()
   init_module_language_server_paths()
@@ -27,6 +29,7 @@ proc shutdownModules*() =
   when declared(shutdown_module_language_server_ctags): shutdown_module_language_server_ctags()
   when declared(shutdown_module_language_server_regex): shutdown_module_language_server_regex()
   when declared(shutdown_module_workspace_edit): shutdown_module_workspace_edit()
+  when declared(shutdown_module_command_component): shutdown_module_command_component()
   when declared(shutdown_module_terminal): shutdown_module_terminal()
   when declared(shutdown_module_language_server_lsp): shutdown_module_language_server_lsp()
   when declared(shutdown_module_language_server_paths): shutdown_module_language_server_paths()
