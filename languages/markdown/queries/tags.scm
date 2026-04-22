d91d328839532ee9ac9657449382bcb986c57464
Nimaoth <nicolas.otremba@web.de>
2026-04-22 13:57:50 +0200

    Add markdown inline queries [no ci]

---
+++ languages/markdown/queries/tags.scm
@@ -0,0 +1,7 @@
+
+(section (atx_heading (atx_h1_marker) heading_content: (_) @name)) @definition.class
+(section (atx_heading (atx_h2_marker) heading_content: (_) @name)) @definition.class
+(section (atx_heading (atx_h3_marker) heading_content: (_) @name)) @definition.class
+(section (atx_heading (atx_h4_marker) heading_content: (_) @name)) @definition.class
+(section (atx_heading (atx_h5_marker) heading_content: (_) @name)) @definition.class
+(section (atx_heading (atx_h6_marker) heading_content: (_) @name)) @definition.class
